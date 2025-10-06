40a45960c0de9afd47557da1da5a877ff228f562
Alejandra Becerra <alejabecerra300@gmail.com>
2025-10-06 13:06:06 -0500

    actualiza las contraseñas encriptadas de los insert inicales

--- BD/DML.sql
+++ BD/DML.sql
@@ -18,12 +18,12 @@ INSERT INTO usuario (
     tipo_permiso,
     estado
 ) VALUES 
-('Juan Pablo Cuellar Vanegas','jpcuellarva1@gmail.com', '312111331','jPablo1*', 1211223456, 'paciente','activo'),
-('Kevin Steven Parra Norena','ksparran1@gmail.com','312222333','k-Ste133', 1294919423,'paciente','activo'),
-('Maria Alejandra Nunez Chaucanes','manunezc1@gmail.com','312333444','m_aleja*', 1112445690, 'medico','activo'),
-('Carlos Caicedo Perez', 'carlitoscaicedo4@gmail.com','312444555','carPe-12', 1233399255, 'medico','activo'),
-('Alejandra Becerra Bellaiza','abecerrabe1@gmail.com','312345678','aleja12*', 1112940289, 'administrador','activo'),
-('Javier Arias','jarias3@gmail.com', '319383929', 'jArias3*', 1010239312,'administrador','activo');
+('Juan Pablo Cuellar Vanegas','Jpcuellarva1@gmail.com', '312111331','$2y$10$9lfRoou0v4SC9j5LNvZ9a.HBNZVG23g5DkF4myXjjLIGS16kO9Xu2', 1211223456, 'paciente','activo'),
+('Kevin Steven Parra Norena','ksparran1@gmail.com','312222333','$2y$10$VWjU.XvRMh7FQ5NdCYvku.m6skpoCm4kmpOsHxhmuAzLsOHjumwk6', 1294919423,'paciente','activo'),
+('Maria Alejandra Nunez Chaucanes','manunezc1@gmail.com','312333444','$2y$10$dnb.QuSZEjtiGRSMKoBWC.DpvmnZC1tNShziW/fc.EauIA9MTuSJO*', 1112445690, 'medico','activo'),
+('Carlos Caicedo Perez', 'carlitoscaicedo4@gmail.com','312444555','$2y$10$YvIAmVB5gAQOiIT31n3TVuEnYRwhz/cMzSQ801voiC7LXpzTuCwEO', 1233399255, 'medico','activo'),
+('Alejandra Becerra Bellaiza','abecerrabe1@gmail.com','312345678','$2y$10$tE/z8DikKTi0.4DIahGefenfbwMnZaj9Waz6Tm9sY5JZoKa5Ovgh.', 1112940289, 'administrador','activo'),
+('Javier Arias','jarias3@gmail.com', '319383929', '$2y$10$BUDV1.JXABJhFx.milFtD.Byz25.O0K8LASnIdxtGI1a6lyq0VKL.', 1010239312,'administrador','activo');
 
 
 --crear pacientes
